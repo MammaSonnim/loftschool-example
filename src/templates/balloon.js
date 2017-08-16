dda2d7b6701c74ccab02327d39e109d7e00ff6eb
Yana Kriklivaya <msonnim@gmail.com>
2017-08-16 10:51:48 +0300

    module9 add styles

--- src/templates/balloon.js
+++ src/templates/balloon.js
@@ -4,8 +4,8 @@ module.exports = '' +
             '<h2 class="geo-object__address">{{ properties.address }}</h2>' +
             '<button class="geo-object__close" id="close">Закрыть</button>' +
         '</header>' +
-        '<div class="container">' +
-            '<ul class="geo-object__reviews-wrapper">' +
+        '<div class="geo-object__main">' +
+            '<ul class="geo-object__reviews">' +
             '{% if properties.reviews %}' +
                 '{% for review in properties.reviews %}' +
                     '<li class="review"><span class="review__author">{{ review.author }}</span><span class="review__place">{{ review.place }}</span><span class="review__date">{{ review.date }}</span><span class="review__text">{{ review.text }}</span></li>' +
@@ -15,13 +15,12 @@ module.exports = '' +
             '{% endif %}' +
             '</ul>' +
 
-            '<hr>' +'<hr>' +
-
             '<form action="" class="geo-object__form form" id="form">' +
-                '<input type="text" class="form__field field field_name" id="name">' +
-                '<input type="text" class="form__field field field_place" id="place">' +
-                '<textarea name="" cols="30" rows="10" class="form__field field field_text" id="text"></textarea>' +
-                '<button type="submit" class="form__btn btn" id="submit"></button>' +
+                '<h3 class="form__header">Ваш отзыв</h3>' +
+                '<input type="text" class="form__field field field_oneliner" id="name" placeholder="Ваше имя">' +
+                '<input type="text" class="form__field field field_oneliner" id="place" placeholder="Укажите место">' +
+                '<textarea name="" cols="30" rows="10" class="form__field field field_text" id="text" placeholder="Поделитесь впечатлениями"></textarea>' +
+                '<button type="submit" class="form__btn btn" id="submit">Добавить</button>' +
             '</form>' +
         '</div>' +
     '</section>';
